8c0855c6e4f2d0adf2702b347199f5df2f7c9ebe
daprieu <daprieu@gmail.com>
2021-03-01 11:18:05 -0600

    added animalList and AnimalProvider component

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,7 +2,8 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { LocationCard } from "./location/Location"
-import { AnimalCard } from "./animal/AnimalCard"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { AnimalList } from "./animal/AnimalList"
 import { CustomerCard } from "./customer/Customer"
 import { EmployeeCard } from "./employees/Employees"
 
@@ -27,15 +28,15 @@ export const ApplicationViews = () => {
             </Route>
             
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-            <h2>Animals</h2>
-                <article className="animals">
-                    <AnimalCard />
-                    <AnimalCard />
-                    <AnimalCard />
-                </article>
-            </Route>
-
+            {/* Note that the <AnimalList> component is a child of the <AnimalProvider> component. 
+            It is crucial that you wrap components that need data with the provider component that exposes 
+            that data in JSX. You can wrap a component in as many providers as needed. */}
+            <AnimalProvider>
+                <Route path="/animals">
+                <h2>Animals</h2>
+                    <AnimalList />
+                </Route>
+            </AnimalProvider>
             {/* Render the animal list when http://localhost:3000/animals */}
             <Route path="/customers">
                 <h2>Customers</h2>
---
+++ src/components/animal/AnimalProvider.js
@@ -0,0 +1,62 @@
+//This code imports the main React library, and two functions that it exports.
+//We will useState to hold and set the array of animals.
+import React, { useState, createContext } from "react"
+
+// The context is imported and used by individual components that need data
+// A context stores a certain kind of data to be used in your application. 
+// Therefore, when you create a data provider component in React, 
+// you need to create a context.
+export const AnimalContext = createContext()
+//Nothing is stored in the context when it's defined. At this point, 
+//it's just an empty warehouse waiting to be filled.
+
+
+// This component establishes what data can be used.
+//Now that the required functions are imported, and an empty context is created, 
+//it's time to define the data provider component that will allow other 
+//components to use the data in the context.
+export const AnimalProvider = (props) => {
+    //Next, you will use the useState() hook to define a variable 
+    //that holds the state of the component, and a function that updates it.
+    const [animals, setAnimals] = useState([])
+//Here's what the State hook is doing for you with a single line of code.
+// Define the variable which will hold the data.
+//let animals = []
+// Define the function to be used to modify that state.
+//const setAnimals = animalsData => {
+//     if (animalsData !== null && Array.isArray(animalsData)) {
+//         animals = animalsData
+//     }
+// }
+
+    const getAnimals = () => {
+        return fetch("http://localhost:8088/animals?_expand=location")
+        .then(res => res.json())
+        .then(setAnimals)
+    }
+
+    const addAnimal = animalObj => {
+        return fetch("http://localhost:8088/animals", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(animalObj)
+        })
+        .then(getAnimals)
+    }
+
+    /*
+        You return a context provider which has the
+        `animals` state, `getAnimals` function,
+        and the `addAnimal` function as keys. This
+        allows any child elements to access them.
+    */
+    return (
+        <AnimalContext.Provider value={{
+            animals, getAnimals, addAnimal
+        }}>
+            {props.children}
+        </AnimalContext.Provider>
+    )
+}
