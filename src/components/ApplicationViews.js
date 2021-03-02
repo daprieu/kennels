e21caf8d6e72403e5805d5bf4664d43ec6fe3efc
daprieu <daprieu@gmail.com>
2021-03-02 08:56:49 -0600

    Animal form saves new animal

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { LocationProvider } from "./location/LocationProvider"
 import { LocationList } from "./location/LocationList"
+import { AnimalForm } from "./animal/AnimalForm"
 import { AnimalProvider } from "./animal/AnimalProvider"
 import { AnimalList } from "./animal/AnimalList"
 import { CustomerProvider } from "./customer/CustomerProvider"
@@ -33,14 +34,19 @@ export const ApplicationViews = () => {
             It is crucial that you wrap components that need data with the provider component that exposes 
             that data in JSX. You can wrap a component in as many providers as needed. */}
             <AnimalProvider>
-            <LocationProvider>
-                <CustomerProvider>
-                    <Route exact path="/animals">
-                        <h2>Animals</h2>
-                        <AnimalList />
-                    </Route>
-                </CustomerProvider>
-            </LocationProvider>
+                <LocationProvider>
+                    <CustomerProvider>
+                        {/* Note the addition of "exact" now that we have an additional route 
+                        with "/animals" in it below this Route: "/animals/create" */}
+                        <Route exact path="/animals">
+                            <h2>Animals</h2>
+                            <AnimalList />
+                        </Route>
+                        <Route path="/animals/create">
+                            <AnimalForm />
+                        </Route>
+                    </CustomerProvider>
+                </LocationProvider>
             </AnimalProvider>
             {/* Render the animal list when http://localhost:3000/animals */}
             <CustomerProvider>
